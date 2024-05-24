6dee2ebc1d199c9e809a3d0fd489e889646424d0
Super User <root@server-home.home>
2024-05-24 17:57:58 +0200

    update views/view_title.sh

--- views/view_title.sh
+++ views/view_title.sh
@@ -1,11 +1,10 @@
-#! /bin/bash
-
 title_menu () {
     # Show title menu
+    echo -e "\n"
     title_text="$@"
     echo -e $title_text
     for i in `seq ${#title_text}`; do echo -n "#";done
-    echo -e "\n\n"
+    echo -e "\n"
 }
 
 sub_title () {
